ed925166584312460305f4375df1664ada04cc00
Samuel Reed <samuel.trace.reed@gmail.com>
2016-05-17 18:57:19 -0500

    Reduce dependency tree by downloading flow binary manually.

--- test.js
+++ test.js
@@ -6,10 +6,24 @@
  * LICENSE file in the root directory of this source tree. An additional grant
  * of patent rights can be found in the PATENTS file in the same directory.
  */
+import path from 'path';
 import test from 'ava';
-import binCheck from 'bin-check';
-import flow from './';
+import rimraf from 'rimraf';
+import flow from './lib';
 
-test('returns path to binary and verify that it is working', async t => {
-	t.true(await binCheck(flow, ['--version']));
+test.serial.cb('installs', t => {
+	rimraf.sync(path.join(__dirname, 'vendor'));
+	t.plan(1);
+	flow.install(err => {
+		t.true(!err);
+		t.end();
+	});
+});
+
+test('returns path to binary and verify that it is working', t => {
+	const cp = flow.runSync(['version']);
+	t.true(cp.error === undefined);
+	t.true(cp.status === 0);
+	const output = cp.stdout.toString().trim();
+	t.true(output.slice(-flow.VERSION.length) === flow.VERSION);
 });
