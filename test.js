fb98f1b74acdb69b7ce7ab5a8314b758c9155544
Valentin Agachi <github-com@agachi.name>
2017-03-09 01:31:25 +0000

    Fork package with linux binary only

--- test.js
+++ test.js
@@ -9,19 +9,8 @@
  */
 var fs = require('fs');
 
-// osx
-process.platform = 'darwin';
-delete require.cache[require.resolve('./')];
-fs.statSync(require('./'));
-
 // linux
 process.platform = 'linux';
-process.arch = 'x64'
-delete require.cache[require.resolve('./')];
-fs.statSync(require('./'));
-
-// windows
-process.platform = 'win32';
 process.arch = 'x64'
 delete require.cache[require.resolve('./')];
 fs.statSync(require('./'));
